25aa299af11d6b10fb42479690f4504acbea506a
irsol <irinywka@gmail.com>
2020-02-03 16:38:00 +0100

    Added Day 4

--- Day 4: Class vs. Instance.py
+++ Day 4: Class vs. Instance.py
@@ -11,3 +11,21 @@ is not valid, setting age to 0.. In addition, you must write the following insta
     - If age >= 13 and age < 18, print You are a teenager..
     - Otherwise, print You are old..
 """
+
+class Person:
+    def __init__(self,initialAge):
+        # Add some more code to run some checks on initialAge
+    def amIOld(self):
+        # Do some computations in here and print out the correct statement to the console
+    def yearPasses(self):
+        # Increment the age of the person in here
+
+t = int(input())
+for i in range(0, t):
+    age = int(input())         
+    p = Person(age)  
+    p.amIOld()
+    for j in range(0, 3):
+        p.yearPasses()       
+    p.amIOld()
+    print("")
