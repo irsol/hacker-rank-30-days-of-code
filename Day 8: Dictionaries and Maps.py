c473b90585b4b81c2a62a128e37609763bfb6fe1
irsol <irinywka@gmail.com>
2020-02-08 17:04:21 +0100

    Added solution to Day 8

--- Day 8: Dictionaries and Maps.py
+++ Day 8: Dictionaries and Maps.py
@@ -7,3 +7,17 @@ name=phoneNumber; if an entry for name is not found, print Not found instead.
 
 Note: Your phone book should be a Dictionary/Map/HashMap data structure.
 """
+
+n = int(input())
+phoneBook = dict()
+
+for i in range(0, n):
+  name, number = input().split()
+  phoneBook[name] = number
+
+for j in range(0, n):
+  name = input()
+  if name in phoneBook:
+      print("{}={}".format(name, phoneBook[name]))
+  else:
+      print("Not found")
