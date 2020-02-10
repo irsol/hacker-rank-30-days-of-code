5b2854dc5876dd8a39e31623e600d99ca0f42e77
irsol <irinywka@gmail.com>
2020-02-10 18:41:07 +0100

    Added description to Day 10

---
+++ Day 10: Binary Numbers.py
@@ -0,0 +1,23 @@
+"""
+Task
+Given a base-10 integer,n , convert it to binary (base-2). Then find and print the base-10 integer
+denoting the maximum number of consecutive 1's in n's binary representation.
+
+Sample Input 1
+5
+Sample Output 1
+1
+
+Sample Input 2
+13
+Sample Output 2
+2
+
+Explanation
+
+Sample Case 1:
+The binary representation of 5 is 101, so the maximum number of consecutive 1's is 1.
+
+Sample Case 2:
+The binary representation of  is 13, 1101 so the maximum number of consecutive 1's is 2.
+"""
