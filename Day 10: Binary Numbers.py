063e1d05f32a3394ba4e812fc9c8a548632ad75a
irsol <irinywka@gmail.com>
2020-02-10 20:30:10 +0100

    Added solution to Day 10

--- Day 10: Binary Numbers.py
+++ Day 10: Binary Numbers.py
@@ -21,3 +21,25 @@ The binary representation of 5 is 101, so the maximum number of consecutive 1's 
 Sample Case 2:
 The binary representation of  is 13, 1101 so the maximum number of consecutive 1's is 2.
 """
+
+def convertBinaryNum(n):
+  # Count max consecutive 1s
+    maxCon = 0
+  # Count cosecutive 1s
+    countCon = 0
+
+    while n > 0:
+        remainder = n % 2
+        if remainder == 1:
+            countCon += 1
+            if countCon > maxCon:
+                maxCon = countCon
+        else:
+            countCon = 0
+        n = n // 2
+    return maxCon
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print(convertBinaryNum(n))
